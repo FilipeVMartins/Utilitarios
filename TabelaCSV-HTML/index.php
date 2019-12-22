378c289629c71a0db9c100d765dc1bfd2c18c311
FilipeVMartins <filipe_2021@hotmail.com>
2019-12-21 21:43:42 -0300

    Mudar_Styles

--- TabelaCSV-HTML/index.php
+++ TabelaCSV-HTML/index.php
@@ -5,18 +5,16 @@ include_once("Model.php");
 header("Content-Type: text/html; charset=ISO-8859-1",true);
 
 
-//entrada inicial dos dados
-$arquivoo = new Model("arquivo.csv", array(   "nome",
+//entrada inicial dos dados, primeiro parametro é o caminho do arquivo .csv, e o segundos as colunas requeridas do mesmo
+$arquivoo = new Model("arquivo.csv", array(     "nome",
                                                 "sobrenome",
                                                 "cpf",
-                                                "email",
                                                 "datanascimento",
-                                                "genero",
-                                                "tiposanguineo",
-                                                "endereco",
+                                                "genero",             // <- exemplos de possíveis colunas de um arquivo .csv
+                                                "cep",
                                                 "cidade",
                                                 "estado",
-                                                "cep"));
+                                                "email"));
 //gera a matriz que alimentará a tabela, onde o primeiro índice [] são as linhas e o segundo índice [] são as colunas de cada linha da tabela.
 $matriz = $arquivoo->gerarMatriz();
 //gera a array que alimentará cabeçalho e rodapé
@@ -28,24 +26,27 @@ $colunas = $arquivoo->getColunasrequeridas();
     <head>
         <meta charset="ISO-8859-1">
         <title>Tabela CSV-HTML</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css" type="text/css">
+        
     </head>
         
     <body>
-        <table>
+        <table class="table">
             <caption>Tabela CSV-HTML</caption>
-            
+
             <thead>
-                <tr>
+                <tr class="thead-dark">
             <?php for ($coluna = 0; $coluna < count($colunas); $coluna++){  //gera o cabeçalho da tabela?>
-                    <td><?php echo $colunas[$coluna];?></td>
+                    <th scope="col"><?php echo $colunas[$coluna];?></th>
             <?php }?>
                 </tr>               
             </thead>
         
             <tfoot>
             <?php for ($coluna = 0; $coluna < count($colunas); $coluna++){   //gera o rodapé da tabela?>
-                    <td><?php echo $colunas[$coluna];?></td>
+                    <th scope="col"><?php echo $colunas[$coluna];?></th>
             <?php }?>                         
             </tfoot>
             
@@ -53,7 +54,7 @@ $colunas = $arquivoo->getColunasrequeridas();
             <?php for ($matrizlinha = 1; $matrizlinha < count($matriz); $matrizlinha++){ //gera as linhas da tabela?> 
                 <tr>
                 <?php for ($matrizcoluna = 0; $matrizcoluna < count($matriz[$matrizlinha]); $matrizcoluna++){ //gera as colunas de cada linha da tabela?>
-                    <td id="linha"><?php echo $matriz[$matrizlinha][$matrizcoluna];?></td>
+                    <td><?php echo $matriz[$matrizlinha][$matrizcoluna];?></td>
                 <?php }?>
                 </tr>
             <?php }?>
@@ -61,6 +62,11 @@ $colunas = $arquivoo->getColunasrequeridas();
             
         </table>
     </body>
+    
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </html>
 
 
